426a23134384abbbefc346331e545a73314bb081
Tiny999 <salamiabbas3@gmail.com>
2020-09-04 14:32:39 +0100

    post request method

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const http = new easyHTTP();
 
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', 
-function(err, posts){
+// http.get('https://jsonplaceholder.typicode.com/posts', 
+// function(err, posts){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+
+// POST Request
+const data = {
+  title: "Custom Post",
+  body: "This is the body of my custom post"
+};
+
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
   if(err){
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
   }
-});
+})
